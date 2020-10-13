5dc00a38be3e213fa65ae67529344b4ec4fb58d2
wesleysrael <wesley12israel@gmail.com>
2020-10-12 23:17:47 -0300

    Remove djangogirlsvenv

--- djangogirlsvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\administrador\desktop\projeto-pa\djangogirls\djangogirlsvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
